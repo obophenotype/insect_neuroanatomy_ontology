2db6f775db8468b8cc4de2969b6f741b420e3f6b
David Osumi-Sutherland <dosumis@gmail.com>
2016-09-07 10:52:57 +0100

    Update gen_mapping_table.py

--- src/gen_mapping_table.py
+++ src/gen_mapping_table.py
@@ -1,18 +1,22 @@
-
-# coding: utf-8
-
-# In[35]:
+from owltools.graph import OWLGraphWrapper
+from owl2pdm_tools import ont_manager
+import warnings
+#from tsv2pdm import rcd
 
 import requests
 r = requests.get('https://insectbraindb.org/api/v1/brain_region/')
 ibd_neuropils = r.json()
 import re
+
 # Each term in first level of list is a block.  Make this part of adult brain (UBERON_6003624)
 # heirarchy underneath = partonomy - single inheritance.
 # Need to merge left and right (do this on short hand)
 # So can make table
 # ibdb_id | ibd_name | ibdb_abbv | uberon term name (TBA) | part_of | BrainName ABBV| FBbt term
-print("ibdb_id\tibd_name\tibdb_abbv\tuberon term name (TBA)\tpart_of\tBrainName ABBV\tFBbt term")
+print "\t".join(['ibdb_id', 'ibdb_name', 'ibdb_abbv', 
+                'Uberon_label', 'part_of', 'BrainName_abbv',
+                'FBbt_id', 'FBbt_name'])
+
 
 def name_gen(n):
     """Generates Uberon term name by matching up to '-' 
@@ -22,7 +26,7 @@ def name_gen(n):
     if U_name_m:
         return U_name_m.group(1).strip().lower()
     else:
-        return n
+        return n.lower()
     
 def abbv_gen(a):
     m = re.match('(.+)-(l|r)', a)
@@ -30,31 +34,58 @@ def abbv_gen(a):
         return m.group(1)
     else:
         return a
-    
-def print_row(j, superpart_name):
-    ID = str(j['id'])
-    uname = name_gen(j['name'])
-    u_abbv = abbv_gen(j['abbreviation'])
-    print ("\t".join([ID, j['name'], j['abbreviation'], uname, superpart_name, u_abbv]))
-    return uname
 
+class row_gen():
+    def __init__(self, ont_path):
+        self.om = ont_manager(file_path=ont_path)
+        self.lookup = self.gen_lookup()
+    
+    def gen_lookup(self):
+        cs = self.om.ont.getClassesInSignature()
+        BrainName_syns = {}
+        BrainName_FBbt_lookup = {}
+        
+        for c in cs:
+            sf = self.om.bi_sfp.getShortForm(c)
+            subs = self.om.get_subsets(owl_entity_sfid = sf)
+            if 'BrainName' in subs:
+                syns = self.om.get_annotations(owl_entity_sfid = sf,
+                                                        owl_annotation_property_sfid = 'hasExactSynonym')
+                BrainName_syns[sf] = syns
+                for s in syns:
+                    if s not in BrainName_FBbt_lookup:
+                        BrainName_FBbt_lookup[s] = []
+                    BrainName_FBbt_lookup[s].append(sf)
+        return BrainName_FBbt_lookup
+
+    def print_row(self, j, superpart_name):
+        ID = str(j['id'])
+        uname = name_gen(j['name'])
+        u_abbv = abbv_gen(j['abbreviation'])
+        FBbt = ''
+        FBbt_label = ''
+        if u_abbv in self.lookup.keys():
+            if len(self.lookup[u_abbv]) == 1:
+                FBbt = self.lookup[u_abbv][0]
+                FBbt_label = self.om.get_labels(FBbt)[0]
+            else:
+                warnings.warn('%s %s has multiple matches: %s' % (uname, u_abbv, (str(self.lookup[u_abbv]))))
+        print "\t".join([ID, j['name'], j['abbreviation'], uname, superpart_name, 
+                         u_abbv, FBbt, FBbt_label])        
+        return uname
+
+
+rg = row_gen('/repos/drosophila-anatomy-developmental-ontology/fbbt/releases/fbbt-simple.owl')
+ 
+r = requests.get('https://insectbraindb.org/api/v1/brain_region/')
+ibd_neuropils = r.json()            
 
 for b in ibd_neuropils:
-    nbu = print_row(b, 'adult brain')
+    nbu = rg.print_row(j = b, superpart_name='adult brain')
     for n in b['neuropils']:
-        nu = print_row(n, nbu)
+        nu = rg.print_row(j = n, superpart_name=nbu)
         for ns in n['subregions']:
-            print_row(n, nu)
+            rg.print_row(j = ns, superpart_name=nu)
 
-    
-
-
-# In[ ]:
-
-
-
-
-# In[ ]:
-
-
-
+                    
+         
